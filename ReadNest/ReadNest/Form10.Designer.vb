d194bdc1d78793e8f0851a635cdab1da07cb804e
Caesar Tresna Andika <caesar.tresna@ti.ukdw.ac.id>
2025-02-26 18:43:54 +0700

    Upload_UI_3

---
+++ ReadNest/ReadNest/Form10.Designer.vb
@@ -0,0 +1,384 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form10
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form10))
+        PictureBox10 = New PictureBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        PictureBox8 = New PictureBox()
+        lblDiscover = New Label()
+        PictureBox1 = New PictureBox()
+        lblMenu = New Label()
+        lblLogout = New Label()
+        lblFavorite = New Label()
+        lblMyLibrary = New Label()
+        lblCategory = New Label()
+        Label1 = New Label()
+        PictureBox6 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label7 = New Label()
+        Label5 = New Label()
+        Label2 = New Label()
+        txtProfilePassword = New TextBox()
+        txtProfileUsername = New TextBox()
+        PictureBox9 = New PictureBox()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.BackColor = SystemColors.Info
+        PictureBox10.BackgroundImage = CType(resources.GetObject("PictureBox10.BackgroundImage"), Image)
+        PictureBox10.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox10.Location = New Point(13, 254)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(28, 28)
+        PictureBox10.TabIndex = 46
+        PictureBox10.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = SystemColors.Info
+        Label4.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label4.ImageAlign = ContentAlignment.TopRight
+        Label4.Location = New Point(47, 265)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(131, 17)
+        Label4.TabIndex = 45
+        Label4.Text = "Book Borrowing"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = SystemColors.Info
+        Label3.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label3.ImageAlign = ContentAlignment.TopRight
+        Label3.Location = New Point(47, 299)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(103, 17)
+        Label3.TabIndex = 44
+        Label3.Text = "My Statistic"
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = SystemColors.Info
+        PictureBox8.BackgroundImage = CType(resources.GetObject("PictureBox8.BackgroundImage"), Image)
+        PictureBox8.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox8.Location = New Point(13, 288)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(28, 28)
+        PictureBox8.TabIndex = 43
+        PictureBox8.TabStop = False
+        ' 
+        ' lblDiscover
+        ' 
+        lblDiscover.AutoSize = True
+        lblDiscover.BackColor = SystemColors.Info
+        lblDiscover.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblDiscover.ImageAlign = ContentAlignment.TopRight
+        lblDiscover.Location = New Point(47, 127)
+        lblDiscover.Name = "lblDiscover"
+        lblDiscover.Size = New Size(74, 17)
+        lblDiscover.TabIndex = 41
+        lblDiscover.Text = "Discover"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.Info
+        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
+        PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox1.Location = New Point(13, 116)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(28, 28)
+        PictureBox1.TabIndex = 40
+        PictureBox1.TabStop = False
+        ' 
+        ' lblMenu
+        ' 
+        lblMenu.AutoSize = True
+        lblMenu.BackColor = SystemColors.Info
+        lblMenu.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblMenu.ImageAlign = ContentAlignment.TopRight
+        lblMenu.Location = New Point(13, 85)
+        lblMenu.Name = "lblMenu"
+        lblMenu.Size = New Size(51, 17)
+        lblMenu.TabIndex = 39
+        lblMenu.Text = "Menu"
+        ' 
+        ' lblLogout
+        ' 
+        lblLogout.AutoSize = True
+        lblLogout.BackColor = SystemColors.Info
+        lblLogout.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblLogout.ImageAlign = ContentAlignment.TopRight
+        lblLogout.Location = New Point(47, 448)
+        lblLogout.Name = "lblLogout"
+        lblLogout.Size = New Size(63, 17)
+        lblLogout.TabIndex = 38
+        lblLogout.Text = "Logout"
+        ' 
+        ' lblFavorite
+        ' 
+        lblFavorite.AutoSize = True
+        lblFavorite.BackColor = SystemColors.Info
+        lblFavorite.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblFavorite.ImageAlign = ContentAlignment.TopRight
+        lblFavorite.Location = New Point(47, 231)
+        lblFavorite.Name = "lblFavorite"
+        lblFavorite.Size = New Size(73, 17)
+        lblFavorite.TabIndex = 37
+        lblFavorite.Text = "Favorite"
+        ' 
+        ' lblMyLibrary
+        ' 
+        lblMyLibrary.AutoSize = True
+        lblMyLibrary.BackColor = SystemColors.Info
+        lblMyLibrary.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblMyLibrary.ImageAlign = ContentAlignment.TopRight
+        lblMyLibrary.Location = New Point(47, 195)
+        lblMyLibrary.Name = "lblMyLibrary"
+        lblMyLibrary.Size = New Size(81, 17)
+        lblMyLibrary.TabIndex = 36
+        lblMyLibrary.Text = "My Notes"
+        ' 
+        ' lblCategory
+        ' 
+        lblCategory.AutoSize = True
+        lblCategory.BackColor = SystemColors.Info
+        lblCategory.Font = New Font("UD Digi Kyokasho NP-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        lblCategory.ImageAlign = ContentAlignment.TopRight
+        lblCategory.Location = New Point(47, 161)
+        lblCategory.Name = "lblCategory"
+        lblCategory.Size = New Size(80, 17)
+        lblCategory.TabIndex = 35
+        lblCategory.Text = "Category"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Info
+        Label1.Font = New Font("UD Digi Kyokasho NP-B", 20.25F, FontStyle.Bold)
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.ImageAlign = ContentAlignment.TopRight
+        Label1.Location = New Point(13, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(148, 31)
+        Label1.TabIndex = 34
+        Label1.Text = "ReadNest"
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackColor = SystemColors.Info
+        PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), Image)
+        PictureBox6.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox6.Location = New Point(13, 220)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(28, 28)
+        PictureBox6.TabIndex = 33
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.BackColor = SystemColors.Info
+        PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), Image)
+        PictureBox5.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox5.Location = New Point(13, 437)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(28, 28)
+        PictureBox5.TabIndex = 32
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackColor = SystemColors.Info
+        PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), Image)
+        PictureBox4.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox4.Location = New Point(13, 184)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(28, 28)
+        PictureBox4.TabIndex = 31
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = SystemColors.Info
+        PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), Image)
+        PictureBox3.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox3.Location = New Point(13, 150)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(28, 28)
+        PictureBox3.TabIndex = 30
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.Info
+        PictureBox2.BorderStyle = BorderStyle.FixedSingle
+        PictureBox2.Location = New Point(1, -7)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(180, 498)
+        PictureBox2.TabIndex = 42
+        PictureBox2.TabStop = False
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.BackColor = Color.Cornsilk
+        Label7.Font = New Font("UD Digi Kyokasho NK-B", 13.25F, FontStyle.Bold)
+        Label7.Location = New Point(373, 161)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(74, 21)
+        Label7.TabIndex = 51
+        Label7.Text = "Profile"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.Cornsilk
+        Label5.Font = New Font("UD Digi Kyokasho NK-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label5.Location = New Point(376, 260)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(83, 17)
+        Label5.TabIndex = 50
+        Label5.Text = "Password"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Cornsilk
+        Label2.Font = New Font("UD Digi Kyokasho NK-B", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label2.Location = New Point(373, 216)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(86, 17)
+        Label2.TabIndex = 49
+        Label2.Text = "Username"
+        ' 
+        ' txtProfilePassword
+        ' 
+        txtProfilePassword.Font = New Font("Segoe UI", 11F)
+        txtProfilePassword.Location = New Point(465, 260)
+        txtProfilePassword.Name = "txtProfilePassword"
+        txtProfilePassword.PlaceholderText = "Enter your password here"
+        txtProfilePassword.Size = New Size(190, 27)
+        txtProfilePassword.TabIndex = 48
+        ' 
+        ' txtProfileUsername
+        ' 
+        txtProfileUsername.Font = New Font("Segoe UI", 11F)
+        txtProfileUsername.Location = New Point(465, 210)
+        txtProfileUsername.Name = "txtProfileUsername"
+        txtProfileUsername.PlaceholderText = "Enter your username here"
+        txtProfileUsername.Size = New Size(190, 27)
+        txtProfileUsername.TabIndex = 47
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.BackColor = Color.Cornsilk
+        PictureBox9.BorderStyle = BorderStyle.FixedSingle
+        PictureBox9.Location = New Point(345, 127)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(359, 248)
+        PictureBox9.TabIndex = 80
+        PictureBox9.TabStop = False
+        ' 
+        ' Form10
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(934, 486)
+        Controls.Add(Label7)
+        Controls.Add(Label5)
+        Controls.Add(Label2)
+        Controls.Add(txtProfilePassword)
+        Controls.Add(txtProfileUsername)
+        Controls.Add(PictureBox10)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(PictureBox8)
+        Controls.Add(lblDiscover)
+        Controls.Add(PictureBox1)
+        Controls.Add(lblMenu)
+        Controls.Add(lblLogout)
+        Controls.Add(lblFavorite)
+        Controls.Add(lblMyLibrary)
+        Controls.Add(lblCategory)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox9)
+        Name = "Form10"
+        Text = "Form10"
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents lblDiscover As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblMenu As Label
+    Friend WithEvents lblLogout As Label
+    Friend WithEvents lblFavorite As Label
+    Friend WithEvents lblMyLibrary As Label
+    Friend WithEvents lblCategory As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtProfilePassword As TextBox
+    Friend WithEvents txtProfileUsername As TextBox
+    Friend WithEvents PictureBox9 As PictureBox
+End Class
