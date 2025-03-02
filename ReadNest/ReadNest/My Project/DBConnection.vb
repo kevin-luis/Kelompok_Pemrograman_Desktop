cf21f0b2893b46bc2420d9bf95ff4e1f76f4ccc6
Andrea Marie Baikole <117893335+AndreaB54@users.noreply.github.com>
2025-03-03 02:16:15 +0700

    tambah dbconnection

---
+++ ReadNest/ReadNest/My Project/DBConnection.vb
@@ -0,0 +1,23 @@
+﻿Imports MySql.Data.MySqlClient
+Module DBConnection
+    Public conn As MySqlConnection
+    Public cmd As MySqlCommand
+    Public rd As MySqlDataReader
+    Public da As MySqlDataAdapter
+    Public str As String
+
+    Sub koneksi()
+        Try
+            Dim str As String = "Server=localhost;user id=root;password=;database=readnest"
+            conn = New MySqlConnection(str)
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                MessageBox.Show("Koneksi Sukses")
+            Else
+                MessageBox.Show("Koneksi Gagal")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Module
