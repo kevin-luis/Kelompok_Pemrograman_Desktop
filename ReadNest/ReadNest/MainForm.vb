baa03f8960d32565e65d43ddbb839fa144c7557b
Kevin Luis Banamtuan <93308196+kevin-luis@users.noreply.github.com>
2025-03-03 04:30:46 +0700

    rename and add event

---
+++ ReadNest/ReadNest/MainForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+    End Sub
+
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim result As DialogResult = MessageBox.Show("Apakah Anda yakin ingin keluar?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.No Then
+            e.Cancel = True
+        Else
+            Environment.Exit(0)
+        End If
+    End Sub
+End Class
